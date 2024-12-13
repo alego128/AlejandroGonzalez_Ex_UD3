b872608beffbfbb855b4effc5ad71f7b5c3a0976
alego128 <ale.gonzalez.roman@gmail.com>
2024-12-13 09:28:35 +0100

    Revisión 2. INICIALES – Refactoriza

--- src/CrearPartido.java
+++ src/CrearPartido.java
@@ -3,20 +3,20 @@ import java.awt.event.KeyEvent;
 
 
 
-public class ventanaNuevo extends javax.swing.JFrame
+public class CrearPartido extends javax.swing.JFrame
 {
    
     
-    private ventanaPrincipal v;
+    private Apuesta v;
     
    
-    public ventanaNuevo() 
+    public CrearPartido() 
     {
         initComponents();
     }
 
    
-    public ventanaNuevo(ventanaPrincipal ventana) 
+    public CrearPartido(Apuesta ventana) 
     {
         initComponents();
         v = ventana;
@@ -72,7 +72,7 @@ public class ventanaNuevo extends javax.swing.JFrame
 
     public void agregarPartido(String p)
     {
-        v.agregarPartido(p);
+        v.añadePartido(p);
         tPartido.setText("");
         tPartido.requestFocus();
     }
@@ -103,20 +103,21 @@ public class ventanaNuevo extends javax.swing.JFrame
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ventanaNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ventanaNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ventanaNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ventanaNuevo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ventanaNuevo().setVisible(true);
+                new CrearPartido().setVisible(true);
             }
         });
     }
